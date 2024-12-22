720d3cd5dbc7e2213098a24909e6d3080bb8ffd3
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2024-12-22 15:29:47 +0530

    query updates

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  role: { type: String, enum: ['client', 'vendor'], required: true },
+  role: { type: String, enum: ['admin', 'client', 'vendor'], required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
